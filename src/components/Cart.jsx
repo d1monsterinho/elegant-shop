805400eb9ffdec370a435875b08e6d1c9c84b68c
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-05-21 15:13:41 +0300

    Separate CartContextProvider from App

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import CartContext from '../store/cart-context';
+import { CartContext } from '../store/cart-context';
 
 export default function Cart({ items, onUpdateItemQuantity }) {
   const cartContext = useContext(CartContext);
