e8ce84319c59ca75b8f0d1b80feacfa558841f91
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-05-21 14:06:08 +0300

    add context function for autocompletion

--- src/store/cart-context.jsx
+++ src/store/cart-context.jsx
@@ -2,6 +2,8 @@ import { createContext } from 'react';
 
 const CartContext = createContext({
     items: [],
+    addItemToCard: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
 });
 
 export default CartContext;
